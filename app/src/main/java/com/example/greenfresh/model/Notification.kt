273650a1fffd891139b580094eff74ce8eb8b36a
Chanhpd <phamduychanh1904@gmail.com>
2023-05-14 17:43:38 +0700

    Initial commit

--- app/src/main/java/com/example/greenfresh/model/Notification.kt
+++ app/src/main/java/com/example/greenfresh/model/Notification.kt
@@ -1,3 +1,9 @@
 package com.example.greenfresh.model
 
-data class Notification(var id: Int, var date : String,var payment: String, var status: String, var address: String): java.io.Serializable
+data class Notification(
+    var id: Int,
+    var date: String,
+    var payment: String,
+    var status: String,
+    var address: String
+) : java.io.Serializable
